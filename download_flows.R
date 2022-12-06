73a2611ab6f4c20e6c99b39830a62ed2d3c9b82d
Wendy Prudencio <78438945+wprudencio1@users.noreply.github.com>
2022-12-05 22:04:35 -0500

    download_flow
    Data frame for all the years.

---
+++ download_flows.R
@@ -0,0 +1,89 @@
+pr_counties <- tigris::counties(state = "PR", cb = TRUE)
+
+
+# 2015
+pr_counties_flows_2015 <-
+  purrr::map_dfr(
+    pr_counties$COUNTYFP,
+    ~ get_flows(
+      geography = "county",
+      state = "PR",
+      county = .x,
+      year = 2015
+    )
+  )
+
+pr_counties_flows_2015$year <- 2015
+
+
+# 2016}
+pr_counties_flows_2016 <-
+  purrr::map_dfr(
+    pr_counties$COUNTYFP,
+    ~ get_flows(
+      geography = "county",
+      state = "PR",
+      county = .x,
+      year = 2016
+    )
+  )
+
+pr_counties_flows_2016$year <- 2016
+
+
+# 2017}
+pr_counties_flows_2017 <-
+  purrr::map_dfr(
+    pr_counties$COUNTYFP,
+    ~ get_flows(
+      geography = "county",
+      state = "PR",
+      county = .x,
+      year = 2017
+    )
+  )
+
+pr_counties_flows_2017$year <- 2017
+
+# 2018}
+pr_counties_flows_2018 <-
+  purrr::map_dfr(
+    pr_counties$COUNTYFP,
+    ~ get_flows(
+      geography = "county",
+      state = "PR",
+      county = .x,
+      year = 2018
+    )
+  )
+
+pr_counties_flows_2018$year <- 2018
+
+
+# 2019}
+pr_counties_flows_2019 <-
+  purrr::map_dfr(
+    pr_counties$COUNTYFP,
+    ~ get_flows(
+      geography = "county",
+      state = "PR",
+      county = .x,
+      year = 2019
+    )
+  )
+
+pr_counties_flows_2019$year <- 2019
+
+pr_counties_flows_2015_2019 <-
+ bind_rows(
+   pr_counties_flows_2015,
+   pr_counties_flows_2016,
+   pr_counties_flows_2017,
+   pr_counties_flows_2018,
+   pr_counties_flows_2019
+ )
+
+readr::write_rds(pr_counties_flows_2015_2019, "data/pr_counties_flows_2015_2019.rds")
+
+
+
